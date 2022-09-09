fac2363192e580208e9b5dbcb6ee088f1d438d73
Sateesh Kumar <sateeshjumbo@gmail.com>
2022-09-09 19:21:37 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# italy_travel_app
+
+Discover the best attractions Italy has to offer
+
+## App Showcase
---
+++ android/app/src/main/kotlin/com/sateeshjh/italy_travel_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sateeshjh.italy_travel_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
